9ea8d233086af5eda3feaa2db8a81dda2bde002a
Mari Sato <marge0101.ms@gmail.com>
2018-02-23 15:01:58 +0000

    want ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,20 @@
 Rails.application.routes.draw do
   
-  get 'ownerships/create'
-
-  get 'ownerships/destroy'
-
-  get 'items/new'
-
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
+  get 'rankings/want'
 
  root to: 'toppages#index'
   get 'signup', to:'users#new'
   
-  get    'login' ,  to: 'sessions#new'
-  post   'login' ,  to: 'sessions#create'
-  delete 'logout' , to: 'sessions#destroy'
+  get    'login' ,        to: 'sessions#new'
+  post   'login' ,        to: 'sessions#create'
+  delete 'logout' ,       to: 'sessions#destroy'
+  get    'rankings/want', to: 'rankings#want'
   
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :items, only: [:show,:new]
   resources :ownerships, only: [:create, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
